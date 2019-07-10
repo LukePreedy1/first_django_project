99c5925883ead5cb80229b53871fe6c8695ee030
LukePreedy1 <lukepreedy@gmail.com>
2019-07-10 15:39:06 -0400

    finished part 6

--- first_django_project/first_django_project/settings.py
+++ first_django_project/first_django_project/settings.py
@@ -119,3 +119,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = ['/static/polls/']
+STATIC_ROOT = '~/github/first_django_project/first_django_project/polls/static/'
